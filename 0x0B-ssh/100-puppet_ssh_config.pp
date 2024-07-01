3176b30a2bdeab0e0f8c4d652aa4b8834fd9b9b5
abangayoma <abangayoma@gmail.com>
2024-06-30 21:09:42 -0700

    created a puppet file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,17 +1,28 @@
 # Puppet manifest to configure SSH client
 
-file { '/home/root/.ssh':
-  ensure  => directory,
-  owner   => 'your_username',
-  group   => 'your_username',
-  mode    => '0700',
+#file { '/home/root/.ssh':
+  #ensure  => directory,
+  #owner   => 'your_username',
+  #group   => 'your_username',
+ # mode    => '0700',
+#}
+#
+#file { '/home/root/.ssh/config':
+#  ensure  => file,
+#  owner   => 'root',
+#  group   => 'root',
+#  mode    => '0600',
+# content => template('ssh/ssh_config.erb'),
+#}
+# Seting up my client config file
+include stdlib
+
+file_line { 'Turn off passwd auth':
+  path => '/etc/ssh/ssh_config',
+  line => 'PasswordAuthentication no',
 }
 
-file { '/home/root/.ssh/config':
-  ensure  => file,
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0600',
-  content => template('ssh/ssh_config.erb'),
+file_line { 'Delare identity file':
+  path => '/etc/ssh/ssh_config',
+  line => 'IdentityFile ~/.ssh/school',
 }
-
